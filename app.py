dcdda34dae7a5f91e98a2fb1ceea21648c8e8d66
nmabeya18 <143057858+nmabeya18@users.noreply.github.com>
2026-08-24 19:02:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def add_security_headers(response):
     response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
         "script-src 'self'; "
-        "style-src 'self' 'unsafe-inline'; "
+        "style-src 'self'; "
         "img-src 'self' data:; "
         "font-src 'self'; "
         "connect-src 'self'; "
